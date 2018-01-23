ec29d43b0ef64ee6200c6c35b59a28944fa7f7ba
nelzon1 <jake.nelzon@gmail.com>
2018-01-22 19:35:36 -0800

    Fixed issue with the joke text not displaying

--- jokes.html.php
+++ jokes.html.php
@@ -8,16 +8,17 @@
   </head>
   <body>
   <p><a href="?addjoke">Add your own joke</a></p>
-  <p>Here are all the jokes in the database:</p>
     <p>Here are all the jokes in the database:</p>
     <?php foreach ($jokes as $joke): ?>
       <form action="?deletejoke" method="post">1
-      <blockquote><p>
-        <?php echo htmlspecialchars($joke, ENT_QUOTES, 2, 'UTF-8'); ?>
+      <blockquote>
+        <p>
+        <?php echo htmlspecialchars($joke['text'], ENT_QUOTES, 2, 'UTF-8'); ?>
         <input type="hidden" name="id" value="<?php
-          echo $joke['id']; ?>">3
+          echo $joke['id']; ?>"/>3
           <input type="submit" value="Delete"/>4
-      </p></blockquote>
+      </p>
+    </blockquote>
     </form>5
     <?php endforeach; ?>
   </body>
