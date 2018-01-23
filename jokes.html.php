23584eada61030fd98bd0646cc5f9b9726d042a5
nelzon1 <jake.nelzon@gmail.com>
2018-01-22 19:54:44 -0800

    Removed the '2' option in the html special char call. Was throwing errors on the windows xampp server.

--- jokes.html.php
+++ jokes.html.php
@@ -13,7 +13,7 @@
       <form action="?deletejoke" method="post">
       <blockquote>
         <p>
-        <?php echo htmlspecialchars($joke['text'], ENT_QUOTES, 2, 'UTF-8'); ?>
+        <?php echo htmlspecialchars($joke['text'], ENT_QUOTES,  'UTF-8'); ?>
         <input type="hidden" name="id" value="<?php
           echo $joke['id']; ?>"/>
           <input type="submit" value="Delete"/>
